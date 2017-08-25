14320f98898fec0509f91ac9f2c5893dc5df327f
Darrell Tunnell <darrell.tunnell@googlemail.com>
2017-08-25 08:15:39 +0100

    Regression test added for #33

--- src/DotNet.Glob.Tests/GlobTests.cs
+++ src/DotNet.Glob.Tests/GlobTests.cs
@@ -5,29 +5,31 @@ namespace DotNet.Glob.Tests
 {
     public class GlobTests
     {
-       
+
         [Theory]
-        [InlineData("literal", "fliteral", "foo/literal", "literals", "literals/foo")]
-        [InlineData("path/hats*nd", "path/hatsblahn", "path/hatsblahndt")]
-        [InlineData("path/?atstand", "path/moatstand", "path/batstands")]
-        [InlineData("/**/file.csv", "/file.txt")]
-        [InlineData("/*file.txt", "/folder")]
-        [InlineData("Shock* 12", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
-        [InlineData("*Shock* 12", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
-        [InlineData("*ave*2", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
-        [InlineData("*ave 12", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
-        [InlineData("*ave 12", "wave 12/")]
-        [InlineData("C:\\THIS_IS_A_DIR\\**\\somefile.txt", "C:\\THIS_IS_A_DIR\\awesomefile.txt")] // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/27
-        [InlineData("C:\\name\\**", "C:\\name.ext", "C:\\name_longer.ext")] // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/29
-        public void Does_Not_Match(string pattern, params string[] testStrings)
+        [InlineData("literal", false, "fliteral", "foo/literal", "literals", "literals/foo")]
+        [InlineData("path/hats*nd", false, "path/hatsblahn", "path/hatsblahndt")]
+        [InlineData("path/?atstand", false, "path/moatstand", "path/batstands")]
+        [InlineData("/**/file.csv", false, "/file.txt")]
+        [InlineData("/*file.txt", false, "/folder")]
+        [InlineData("Shock* 12", false, "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
+        [InlineData("*Shock* 12", false, "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
+        [InlineData("*ave*2", false, "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
+        [InlineData("*ave 12", false, "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
+        [InlineData("*ave 12", false, "wave 12/")]
+        [InlineData("C:\\THIS_IS_A_DIR\\**\\somefile.txt", false, "C:\\THIS_IS_A_DIR\\awesomefile.txt")] // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/27
+        [InlineData("C:\\name\\**", false, "C:\\name.ext", "C:\\name_longer.ext")] // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/29
+        [InlineData("Bumpy/**/AssemblyInfo.cs", false, "Bumpy.Test/Properties/AssemblyInfo.cs")]      // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/33
+        public void Does_Not_Match(string pattern, bool allowInvalidPathCharcters, params string[] testStrings)
         {
-            var glob = Globbing.Glob.Parse(pattern);
+            GlobParseOptions options = new GlobParseOptions() { AllowInvalidPathCharacters = allowInvalidPathCharcters };
+            var glob = Globbing.Glob.Parse(pattern, options);
             foreach (var testString in testStrings)
             {
                 Assert.False(glob.IsMatch(testString));
             }
         }
-    
+
         [Theory]
         [InlineData("literal", "literal")]
         [InlineData("a/literal", "a/literal")]
@@ -50,22 +52,21 @@ namespace DotNet.Glob.Tests
         [InlineData("/DIR1/*/*", "/DIR1/DIR2/file.txt")]  // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/21
         [InlineData("~/*~3", "~/abc123~3")]  // Regression Test for https://github.com/dazinator/DotNet.Glob/pull/15
         [InlineData("**\\Shock* 12", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
-        [InlineData("**\\*ave*2", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]       
+        [InlineData("**\\*ave*2", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
         [InlineData("**", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12")]
         [InlineData("**", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Shockwave 12.txt")]
         [InlineData("Stuff, *", "Stuff, x")]      // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/31
         [InlineData("\"Stuff*", "\"Stuff")]      // Regression Test for https://github.com/dazinator/DotNet.Glob/issues/32
         public void IsMatch(string pattern, params string[] testStrings)
         {
-            DotNet.Globbing.Glob.Parse("\"Stuff*", new GlobParseOptions() { AllowInvalidPathCharacters = true }).IsMatch("\"Stuff"); // true; 
 
-        GlobParseOptions.Default.AllowInvalidPathCharacters = true;
+            GlobParseOptions.Default.AllowInvalidPathCharacters = true;
             var glob = Globbing.Glob.Parse(pattern);
             foreach (var testString in testStrings)
             {
                 var match = glob.IsMatch(testString);
-                Assert.True(match);                
-                           
+                Assert.True(match);
+
             }
         }
 
@@ -107,12 +108,12 @@ namespace DotNet.Glob.Tests
             var glob = Globbing.Glob.Parse(pattern);
             var resultPattern = glob.ToString();
             Assert.Equal(pattern, resultPattern);
-        }   
+        }
 
 
-      
 
 
-      
+
+
     }
 }
