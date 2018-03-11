b472d7fdfcb6a598acc71a7dd962f2057b948fd1
Darrell Tunnell <darrell.tunnell@googlemail.com>
2018-03-11 15:12:23 +0000

    #30 - Fix versionins issue

--- src/DotNet.Glob/Properties/AssemblyInfo.cs
+++ src/DotNet.Glob/Properties/AssemblyInfo.cs
@@ -1,14 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNet.Glob")]
-[assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
